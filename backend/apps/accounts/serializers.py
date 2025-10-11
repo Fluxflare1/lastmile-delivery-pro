875989ff65529e60de1bf2dd00b9814d5b3d3a0b
Itebimien Momodu <fluxflareltd@gmail.com>
2025-10-11 20:33:11 +0100

    Create serializers.py

---
+++ backend/apps/accounts/serializers.py
@@ -0,0 +1,29 @@
+from rest_framework import serializers
+from django.contrib.auth import authenticate
+from .models import User
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, min_length=6)
+
+    class Meta:
+        model = User
+        fields = ["email", "phone", "user_type", "default_role", "password"]
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        user = authenticate(email=data["email"], password=data["password"])
+        if not user:
+            raise serializers.ValidationError("Invalid credentials")
+        return {"user": user}
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "email", "phone", "user_type", "default_role", "profile_complete", "created_at"]
