06ea55420b77ff61cd3448053d46f86413637789
Itebimien Momodu <fluxflareltd@gmail.com>
2025-10-13 15:51:05 +0100

    Update views.py

--- backend/apps/accounts/views.py
+++ backend/apps/accounts/views.py
@@ -1,23 +1,15 @@
 from rest_framework import generics, permissions
-from rest_framework.response import Response
-from rest_framework_simplejwt.views import TokenObtainPairView
 from .models import User
-from .serializers import UserSerializer, RegisterSerializer
-from .permissions import IsAdminOrSelf
-
-class RegisterView(generics.CreateAPIView):
-    queryset = User.objects.all()
-    serializer_class = RegisterSerializer
-    permission_classes = [permissions.AllowAny]
+from .serializers import UserSerializer
 
 
 class UserListView(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [permissions.IsAdminUser]
+    permission_classes = [permissions.IsAuthenticated]
 
 
-class UserDetailView(generics.RetrieveUpdateAPIView):
+class UserDetailView(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [IsAdminOrSelf]
+    permission_classes = [permissions.IsAuthenticated]
