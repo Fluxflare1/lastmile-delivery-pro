a50d4a9fd817d7d5b918484f10ae356932b1f64e
Itebimien Momodu <fluxflareltd@gmail.com>
2025-10-13 14:42:48 +0100

    Update urls.py

--- backend/apps/accounts/urls.py
+++ backend/apps/accounts/urls.py
@@ -1,14 +1,7 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import RegisterView, LoginView, ProfileView, AddressViewSet
-
-router = DefaultRouter()
-router.register(r'addresses', AddressViewSet, basename='addresses')
+from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    path("register/", RegisterView.as_view(), name="register"),
-    path("login/", LoginView.as_view(), name="login"),
-    path("profile/", ProfileView.as_view(), name="profile"),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
-
-urlpatterns += router.urls
