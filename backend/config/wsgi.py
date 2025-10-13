5783f04c092f2d3c1920310736bd437633470fe0
Itebimien Momodu <fluxflareltd@gmail.com>
2025-10-13 15:13:17 +0100

    Update wsgi.py

--- backend/config/wsgi.py
+++ backend/config/wsgi.py
@@ -1,6 +1,10 @@
+"""
+WSGI config for LastMile Delivery Pro project.
+"""
+
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_wsgi_application()
