553b77cc6dca6f818a069c6f393c2b7be4f17245
Itebimien Momodu <fluxflareltd@gmail.com>
2025-10-15 12:30:08 +0100

    Create layout.tsx

---
+++ frontend/apps/customer/app/layout.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../styles/globals.css";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className="bg-gray-100 text-gray-900">{children}</body>
+    </html>
+  );
+}
