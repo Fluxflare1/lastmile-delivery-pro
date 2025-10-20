eea138ef514cbcdf2428281e2ee7eb0c461c1e19
Itebimien Momodu <fluxflareltd@gmail.com>
2025-10-20 12:23:37 +0100

    Add index file to export multiple services

---
+++ frontend/packages/shared/api/services/index.ts
@@ -0,0 +1,5 @@
+export { default as AuthService } from "./authService";
+export { default as LmdspService } from "./lmdspService";
+export { default as DcsdService } from "./dcsdService";
+export { default as UploadService } from "./uploadService";
+export { default as AnalyticsService } from "./analyticsService";
