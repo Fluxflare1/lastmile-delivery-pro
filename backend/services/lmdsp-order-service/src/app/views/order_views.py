816fd29d9851df2a571b2435efc5dd576dcd032c
Itebimien Momodu <fluxflareltd@gmail.com>
2025-11-19 10:29:35 +0100

    Update order_views.py

--- backend/services/lmdsp-order-service/src/app/views/order_views.py
+++ backend/services/lmdsp-order-service/src/app/views/order_views.py
@@ -1,8 +1,13 @@
 from rest_framework import generics
 from app.models.order import Order
 from app.serializers.order_serializers import OrderSerializer
+from app.tasks.order_tasks import process_new_order
+
 
 class OrderListCreateView(generics.ListCreateAPIView):
+    def perform_create(self, serializer):
+    order = serializer.save()
+    process_new_order.delay(str(order.id))
     queryset = Order.objects.all().order_by("-created_at")
     serializer_class = OrderSerializer
 
@@ -11,3 +16,7 @@ class OrderRetrieveUpdateView(generics.RetrieveUpdateAPIView):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
     lookup_field = "id"
+
+
+
+
